182d79e70542b12126f2c5470f11e59e8d9ce6a8
lius <liuswin@163.com>
2018-05-30 09:08:14 +0800

    fix bug & hello component

---
+++ app/components/hello.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './hello.less'
+
+export default class Component extends React.Component {
+  render() {
+    return (
+      <div className="hello-component">
+        Hello, React
+      </div>
+    );
+  }
+}
--- config/webpack.common.js
+++ config/webpack.common.js
@@ -2,13 +2,13 @@ const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './app/index.js',
   output: {
     filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, '../dist')
   },
   module: {
     rules: [
@@ -17,12 +17,28 @@ module.exports = {
         exclude: /node_modules/,
         loader: 'babel-loader'
       },
+      // {
+      //   test: /\.css$/,
+      //   use: ExtractTextPlugin.extract({
+      //     fallback: 'style-loader',
+      //     use: 'css-loader'
+      //   })
+      // },
       {
-        test: /\.css$/,
-        use: ExtractTextPlugin.extract({
-          fallback: "style-loader",
-          use: "css-loader"
-        })
+        test: /\.(less|css)$/,
+        use: [
+          {
+              loader: "style-loader"
+          }, {
+              loader: "css-loader"
+          }, {
+              loader: "less-loader", options: {
+                  paths: [
+                      path.resolve(__dirname, "node_modules")
+                  ]
+              }
+          }
+        ]
       },
       {
         test: /\.(png|svg|jpg|gif)$/,
@@ -33,9 +49,12 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
-      title: 'Production'
+      title: 'test',
+      template: './index.html',
+      filename: 'index.html'
     }),
-    new ExtractTextPlugin("styles.css"),
+    new ExtractTextPlugin('styles.css'),
+    new webpack.HotModuleReplacementPlugin(),
     // new webpack.optimize.splitChunks({
     //   name: 'common' // 防止重复 指定公共 bundle 的名称
     // })
