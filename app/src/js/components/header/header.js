209004084e57e2fd8f8e43484089bfdef25f2247
lius <liuswin@163.com>
2018-06-04 14:43:00 +0800

    jPlayer player music

--- app/src/js/components/header/header.js
+++ app/src/js/components/header/header.js
@@ -1,14 +1,14 @@
-import React from 'react'
-import './header.less'
-import logo from '../../../static/images/logo.png'
+import React from 'react';
+import './header.less';
+import logo from '../../../static/images/logo.png';
 
 export default class Component extends React.Component {
-    render() {
-        return (
-            <div className="components-header row">
-                <img src={logo} width="40" alt="" className="-col-auto"/>
-                <h1 className="caption">React Music Player</h1>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="components-header row">
+        <img src={logo} width="40" alt="" className="-col-auto" />
+        <h1 className="caption">React Music Player</h1>
+      </div>
+    );
+  }
 }
